5ba2c0730b0223597f834a10fa769ca0e6975bb4
Stephen Kelman <stephenrkelman@gmail.com>
2024-06-02 00:09:09 +0000

    Added functionality for markdown parsing api
    - functionality from https://github.com/sevenjay/cpp-markdown
    - updated api to no longer return optionals, since the cpp-markdown library has
      no explicit failure conditions -- issues will just cause the write to be empty
    - tested build locally (clean_build, clean_coverage, start_docker)
    - unit testing coming in a separate commit
    
    Change-Id: I14deb5e5a650bdb01c4f738291dac92d1520c405

--- include/markdown_parser.h
+++ include/markdown_parser.h
@@ -2,15 +2,13 @@
 #define MARKDOWN_PARSER_H
 
 #include <string>
-#include <optional>
 
 class MarkdownParser {
 public:
     MarkdownParser();
     // treating the string argument as a markdown file, parse into html
-    // if parse is successful, return the html equivalent of the original markdown
-    // otherwise, return std::nullopt
-    std::optional<std::string> parse_markdown(std::string markdown);
+    // return the html equivalent of the original markdown
+    std::string parse_markdown(std::string markdown);
 };
 
 #endif // MARKDOWN_PARSER_H
--- src/markdown_parser.cc
+++ src/markdown_parser.cc
@@ -1,7 +1,17 @@
+#include "constants.h"
 #include "markdown_parser.h"
+
+#include <markdown.h>
+#include <sstream>
+#include <stack>
+#include <iostream>
 
 MarkdownParser::MarkdownParser(){}
 
-std::optional<std::string> parse_markdown(std::string markdown){
-    return markdown;
+std::string MarkdownParser::parse_markdown(std::string markdown){
+  markdown::Document document;
+  document.read(markdown);
+  std::ostringstream html;
+  document.write(html);
+  return html.str();
 }
